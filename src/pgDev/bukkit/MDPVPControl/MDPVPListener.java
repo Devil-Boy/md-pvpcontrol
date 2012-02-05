d179f06cb23f1c257a654d57ce17b7d7fd17a5f1
Devil boy <kws512@hotmail.com>
2012-02-04 22:02:50 -0800

    Fixed to catch EntityDamageEvent

--- src/pgDev/bukkit/MDPVPControl/MDPVPListener.java
+++ src/pgDev/bukkit/MDPVPControl/MDPVPListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.*;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class MDPVPListener implements Listener {
 	MDPVPCMain plugin;
@@ -16,30 +17,34 @@ public class MDPVPListener implements Listener {
 	}
 	
 	@EventHandler
-	public void damageCatcher(EntityDamageByEntityEvent event) {
-		// Disguised cannot attack
-		if (event.getDamager() instanceof Player) {
-			Player attacker = (Player) event.getDamager();
-			if (MobDisguiseAPI.isDisguised(attacker) && !plugin.hasPermissions(attacker, "mdpvpcontrol.candisguiseattack")) {
-				event.setCancelled(true);
-				attacker.sendMessage(ChatColor.RED + "You cannot attack while disguised!");
-			}
-		} else if (event.getDamager() instanceof Projectile) {
-			if (((Projectile) event.getDamager()).getShooter() instanceof Player) {
-				Player attacker = (Player) ((Projectile) event.getDamager()).getShooter();
+	public void damageCatcher(EntityDamageEvent e) {
+		if (e instanceof EntityDamageByEntityEvent) {
+			EntityDamageByEntityEvent event = (EntityDamageByEntityEvent) e;
+			
+			// Disguised cannot attack
+			if (event.getDamager() instanceof Player) {
+				Player attacker = (Player) event.getDamager();
 				if (MobDisguiseAPI.isDisguised(attacker) && !plugin.hasPermissions(attacker, "mdpvpcontrol.candisguiseattack")) {
 					event.setCancelled(true);
 					attacker.sendMessage(ChatColor.RED + "You cannot attack while disguised!");
 				}
+			} else if (event.getDamager() instanceof Projectile) {
+				if (((Projectile) event.getDamager()).getShooter() instanceof Player) {
+					Player attacker = (Player) ((Projectile) event.getDamager()).getShooter();
+					if (MobDisguiseAPI.isDisguised(attacker) && !plugin.hasPermissions(attacker, "mdpvpcontrol.candisguiseattack")) {
+						event.setCancelled(true);
+						attacker.sendMessage(ChatColor.RED + "You cannot attack while disguised!");
+					}
+				}
 			}
-		}
-		
-		// Disguised lose disguise
-		if (!event.isCancelled() && event.getEntity() instanceof Player) {
-			Player defender = (Player) event.getEntity();
-			if (!plugin.hasPermissions(defender, "mdpvpcontrol.nodamagedisguiseloss")) {
-				MobDisguiseAPI.undisguisePlayer(defender);
-				defender.sendMessage(ChatColor.RED + "Your disguise was blown.");
+			
+			// Disguised lose disguise
+			if (!event.isCancelled() && event.getEntity() instanceof Player) {
+				Player defender = (Player) event.getEntity();
+				if (!plugin.hasPermissions(defender, "mdpvpcontrol.nodamagedisguiseloss")) {
+					MobDisguiseAPI.undisguisePlayer(defender);
+					defender.sendMessage(ChatColor.RED + "Your disguise was blown.");
+				}
 			}
 		}
 	}
